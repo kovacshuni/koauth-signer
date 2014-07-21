394bf235643bdbf549f96e37d0a54a103ba8732f
Hunor Kovács <kovacshuni@yahoo.com>
2014-07-22 00:13:26 +0300

    Super compile error

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,6 +1,9 @@
 package controllers
 
+import com.hunorkovacs.koauth.domain.OauthParams._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
 
 import scala.concurrent.Future
 import com.hunorkovacs.koauth.service.consumer.{DefaultConsumerService, ConsumerService}
@@ -9,8 +12,40 @@ object Application extends Controller {
 
   import scala.concurrent.ExecutionContext.Implicits.global
 
+  val requestTokenForm = Form(
+    mapping(
+      "method" -> text,
+      "url" -> text,
+      consumerKeyName -> text,
+      consumerSecretName -> text,
+      timestampName -> text,
+      nonceName -> text,
+      signatureName -> text,
+      signatureMethodName -> text,
+      versionName -> text
+    )(
+      (method,
+       url,
+       consumerKey,
+       consumerSecret,
+       timestamp,
+       nonce,
+       signature,
+       signatureMethod,
+       version,
+       _) =>
+        val l = List((consumerKeyName, consumerKey),
+          (consumerSecretName, consumerSecret))
+        val m = l.toMap
+        com.hunorkovacs.koauth.domain.Request(method, url, List.empty, List.empty, l, m)
+      (r: com.hunorkovacs.koauth.domain.Request) =>
+        Some(r.method, r.urlWithoutParams, r.urlParams, r.bodyParams, r.oauthParamsList, r.oauthParamsMap)
+    )
+
+//  val filledForm = loginForm.fill(User("Bob", 18))
+
   def requestToken = Action.async { request =>
-    DefaultConsumerService.createRequestTokenRequest()
+//    DefaultConsumerService.createRequestTokenRequest()
 
     Future {
       val l = List(("oauth_nonce", "123456"))
