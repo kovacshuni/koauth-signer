f5e4a0ab7efdf9044b4bf5fcf866909f678964f3
Hunor Kovács <kovacshuni@yahoo.com>
2014-07-23 00:45:28 +0300

    Trying to surface signed header to html

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,13 +1,14 @@
 package controllers
 
 import com.hunorkovacs.koauth.domain.OauthParams._
+import com.hunorkovacs.koauth.service.consumer.DefaultConsumerService.{createSignatureBase, createRequestTokenRequest}
 import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
 
 import scala.concurrent.Future
 import com.hunorkovacs.koauth.service.consumer.{DefaultConsumerService, ConsumerService}
-import com.hunorkovacs.koauth.domain.Request
+import com.hunorkovacs.koauth.domain.{OauthParams, Request}
 
 object Application extends Controller {
 
@@ -16,16 +17,23 @@ object Application extends Controller {
   def requestToken = Action.async { request =>
     Future {
       val emptyRequest = Request("", "", List.empty, List.empty, List.empty, Map.empty)
-      Ok(views.html.requestToken(emptyRequest)(requestTokenForm))
+      Ok(views.html.requestToken(requestTokenForm)(""))
     }
   }
 
   def requestTokenPost = Action.async { request =>
-    Future {
-      val requestTokenRequest = requestTokenForm.bindFromRequest()(request).get
-      val filledForm = requestTokenForm.fill(requestTokenRequest)
-      Ok(views.html.requestToken(requestTokenRequest)(filledForm))
+    val postedRequestF = Future(requestTokenForm.bindFromRequest()(request).get)
+    val filledFormF = postedRequestF.map(requestTokenForm.fill)
+    val headerF = postedRequestF flatMap { postedRequest =>
+      val consumerKey = postedRequest.oauthParamsMap(consumerKeyName)
+      val consumerSecret = postedRequest.oauthParamsMap(consumerSecretName)
+      val callback = postedRequest.oauthParamsMap(callbackName)
+      createRequestTokenRequest(postedRequest, consumerKey, consumerSecret, callback)
     }
+    for {
+      filledForm <- filledFormF
+      header <- headerF
+    } yield Ok(views.html.requestToken(filledForm)(header))
   }
 
   val requestTokenForm = Form[Request](
