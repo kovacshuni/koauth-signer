bf8ef249e9e56499e9daa20235b6c10538932766
Hunor Kovács <kovacshuni@yahoo.com>
2014-07-26 20:44:36 +0300

    Authorize complete.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -32,21 +32,21 @@ object Application extends Controller {
   }
 
   def authorize = Action.async { request =>
-    Future(Ok(views.html.authorize(authorizeForm)("")))
+    Future(Ok(views.html.authorize(authorizeForm)("", "")))
   }
 
   def authorizePost = Action.async { implicit request =>
-    Future(authorizeForm.bindFromRequest.get) flatMap { koauthRequest =>
-      val consumerKey = koauthRequest.oauthParamsMap(ConsumerKeyName)
-      val token = koauthRequest.oauthParamsMap(TokenName)
-      val username = koauthRequest.oauthParamsMap(UsernameName)
-      val password = koauthRequest.oauthParamsMap(PasswordName)
-      val complementedRequest = KoauthRequest(Method, koauthRequest.urlWithoutParams, List.empty, List.empty,
-        koauthRequest.oauthParamsList)
-      createAuthorizeRequest(complementedRequest, consumerKey, token, username, password)
+    Future(filterRequest(authorizeForm.bindFromRequest.get)) flatMap { filteredRequest =>
+      val consumerKey = filteredRequest.oauthParamsMap(ConsumerKeyName)
+      val consumerSecret = filteredRequest.oauthParamsMap(ConsumerSecretName)
+      val token = filteredRequest.oauthParamsMap(TokenName)
+      val tokenSecret = filteredRequest.oauthParamsMap(TokenSecretName)
+      val username = filteredRequest.oauthParamsMap(UsernameName)
+      val password = filteredRequest.oauthParamsMap(PasswordName)
+      createAuthorizeRequest(filteredRequest, consumerKey, consumerSecret, token, tokenSecret, username, password)
     } map { requestAndInfo =>
       val completedForm = authorizeForm.fill(requestAndInfo.request)
-      Ok(views.html.authorize(completedForm)(requestAndInfo.header))
+      Ok(views.html.authorize(completedForm)(requestAndInfo.signatureBase, requestAndInfo.header))
     }
   }
 
@@ -101,26 +101,46 @@ object Application extends Controller {
       "method" -> text,
       "url" -> text,
       ConsumerKeyName -> text,
+      ConsumerSecretName -> text,
       TokenName -> text,
+      TokenSecretName -> text,
+      SignatureMethodName -> text,
+      TimestampName -> text,
+      NonceName -> text,
+      VersionName -> text,
       UsernameName -> text,
       PasswordName -> text
     )(fromAuthorizeForm)(toAuthorizeForm)
   )
 
-  private def fromAuthorizeForm(method: String, url: String, consumerKey: String, token: String, username: String,
-                                password: String): KoauthRequest = {
+  private def fromAuthorizeForm(method: String, url: String, consumerKey: String, consumerSecret: String,
+                                tokenName: String, tokenSecret: String, signatureMethod: String, timestamp: String,
+                                nonce: String, version: String, username: String, password: String): KoauthRequest = {
     val list = List((ConsumerKeyName, consumerKey),
-      (TokenName, token),
+      (ConsumerSecretName, consumerSecret),
+      (TokenName, tokenName),
+      (TokenSecretName, tokenSecret),
+      (SignatureMethodName, signatureMethod),
+      (TimestampName, timestamp),
+      (NonceName, nonce),
+      (VersionName, version),
       (UsernameName, username),
       (PasswordName, password))
     KoauthRequest(method, url, List.empty, List.empty, list)
   }
 
-  private def toAuthorizeForm(request: KoauthRequest): Option[(String, String, String, String, String, String)] = {
+  private def toAuthorizeForm(request: KoauthRequest): Option[(String, String, String, String, String, String,
+    String, String, String, String, String, String)] = {
     Some(request.method,
       request.urlWithoutParams,
       request.oauthParamsMap(ConsumerKeyName),
+      request.oauthParamsMap(ConsumerSecretName),
       request.oauthParamsMap(TokenName),
+      request.oauthParamsMap(TokenSecretName),
+      request.oauthParamsMap(SignatureMethodName),
+      request.oauthParamsMap(TimestampName),
+      request.oauthParamsMap(NonceName),
+      request.oauthParamsMap(VersionName),
       request.oauthParamsMap(UsernameName),
       request.oauthParamsMap(PasswordName))
   }
