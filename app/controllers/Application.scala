fee44f57421f43ddce2782e5e35f7e8d014f02e4
Hunor Kovács <kovacshuni@yahoo.com>
2014-07-27 12:45:57 +0300

    All pages work and fine. Let's do a little structuring, refactoring.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -18,7 +18,12 @@ object Application extends Controller {
   val Method = "POST"
 
   def requestTokenGet = Action.async { request =>
-    Future(Ok(views.html.requestToken(requestTokenForm)("", "")))
+    Future(KoauthRequest(Method, "", List.empty, List.empty, List.empty)) flatMap { emptyRequest =>
+      createRequestTokenRequest(emptyRequest, "", "", "")
+    } map { requestAndInfo =>
+      val filledForm = requestTokenForm.fill(requestAndInfo.request)
+      Ok(views.html.requestToken(filledForm)("", ""))
+    }
   }
 
   def requestTokenPost = Action.async { implicit request =>
@@ -34,7 +39,12 @@ object Application extends Controller {
   }
 
   def authorizeGet = Action.async { request =>
-    Future(Ok(views.html.authorize(authorizeForm)("", "")))
+    Future(KoauthRequest(Method, "", List.empty, List.empty, List.empty)) flatMap { emptyRequest =>
+      createAuthorizeRequest(emptyRequest, "", "", "", "", "", "")
+    } map { requestAndInfo =>
+      val filledForm = authorizeForm.fill(requestAndInfo.request)
+      Ok(views.html.authorize(filledForm)("", ""))
+    }
   }
 
   def authorizePost = Action.async { implicit request =>
@@ -53,7 +63,12 @@ object Application extends Controller {
   }
 
   def accessTokenGet = Action.async { request =>
-    Future(Ok(views.html.accessToken(accessTokenForm)("", "")))
+    Future(KoauthRequest(Method, "", List.empty, List.empty, List.empty)) flatMap { emptyRequest =>
+      createAccessTokenRequest(emptyRequest, "", "", "", "", "")
+    } map { requestAndInfo =>
+      val filledForm = accessTokenForm.fill(requestAndInfo.request)
+      Ok(views.html.accessToken(filledForm)("", ""))
+    }
   }
 
   def accessTokenPost = Action.async { implicit request =>
@@ -70,8 +85,13 @@ object Application extends Controller {
     }
   }
 
-  def oauthenticateGet =  Action.async { request =>
-    Future(Ok(views.html.oauthenticate(oauthenticateForm)("", "")))
+  def oauthenticateGet = Action.async { request =>
+    Future(KoauthRequest(Method, "", List.empty, List.empty, List.empty)) flatMap { emptyRequest =>
+      createOauthenticatedRequest(emptyRequest, "", "", "", "")
+    } map { requestAndInfo =>
+      val filledForm = oauthenticateForm.fill(requestAndInfo.request)
+      Ok(views.html.oauthenticate(filledForm)("", ""))
+    }
   }
 
   def oauthenticatePost = Action.async { implicit request =>
@@ -83,7 +103,7 @@ object Application extends Controller {
       createOauthenticatedRequest(filteredRequest, consumerKey, consumerSecret, token, tokenSecret)
     } map { requestAndInfo =>
       val completedForm = oauthenticateForm.fill(requestAndInfo.request)
-      Ok(views.html.oauthenticate.apply(completedForm)(requestAndInfo.signatureBase, requestAndInfo.header))
+      Ok(views.html.oauthenticate(completedForm)(requestAndInfo.signatureBase, requestAndInfo.header))
     }
   }
 
@@ -96,7 +116,7 @@ object Application extends Controller {
       .flatMap(createGeneralSignedRequest)
       .map { requestAndInfo =>
       val completedForm = generalRequestForm.fill(requestAndInfo.request)
-      Ok(views.html.oauthenticate.apply(completedForm)(requestAndInfo.signatureBase, requestAndInfo.header))
+      Ok(views.html.generalRequest(completedForm)(requestAndInfo.signatureBase, requestAndInfo.header))
     }
   }
 
@@ -284,76 +304,91 @@ object Application extends Controller {
       request.oauthParamsMap(VersionName))
   }
 
+  val PostParamName1: String = "postParamName1"
+  val PostParamVal1: String = "postParamVal1"
+  val PostParamName2: String = "postParamName2"
+  val PostParamVal2: String = "postParamVal2"
   val generalRequestForm = Form[KoauthRequest](
     mapping(
-      "method" -> text,
-      "url" -> text,
-      ConsumerKeyName -> text,
-      ConsumerSecretName -> text,
-      TokenName -> text,
-      TokenSecretName -> text,
-      SignatureMethodName -> text,
-      TimestampName -> text,
-      NonceName -> text,
-      VersionName -> text,
-      CallbackName -> text,
-      VerifierName -> text,
-      "postParamName1" -> text,
-      "postParamVal1" -> text,
-      "postParamName2" -> text,
-      "postParamVal2" -> text
+      "method" -> optional(text),
+      "url" -> optional(text),
+      ConsumerKeyName -> optional(text),
+      ConsumerSecretName -> optional(text),
+      TokenName -> optional(text),
+      TokenSecretName -> optional(text),
+      SignatureMethodName -> optional(text),
+      TimestampName -> optional(text),
+      NonceName -> optional(text),
+      VersionName -> optional(text),
+      CallbackName -> optional(text),
+      VerifierName -> optional(text),
+      PostParamName1 -> optional(text),
+      PostParamVal1 -> optional(text),
+      PostParamName2 -> optional(text),
+      PostParamVal2 -> optional(text)
     )(fromGeneralForm)(toGeneralForm)
   )
 
-  private def fromGeneralForm(method: String, url: String, consumerKey: String, consumerSecret: String,
-                                    tokenName: String, tokenSecret: String,
-                                    signatureMethod: String, timestamp: String,
-                                    nonce: String, version: String, callback: String,
-                                    verifier: String, postParamName1: String,
-                                    postParamVal1: String, postParamName2: String,
-                                    postParamVal2: String): KoauthRequest = {
-    def appendIfExists(key: String, value: String, list: List[(String, String)]): List[(String, String)] = {
-      if (value.isEmpty) list
-      else list.::((key, value))
+  private def fromGeneralForm(method: Option[String], url: Option[String], consumerKey: Option[String], consumerSecret: Option[String],
+                                    tokenName: Option[String], tokenSecret: Option[String],
+                                    signatureMethod: Option[String], timestamp: Option[String],
+                                    nonce: Option[String], version: Option[String], callback: Option[String],
+                                    verifier: Option[String], postParamName1: Option[String],
+                                    postParamVal1: Option[String], postParamName2: Option[String],
+                                    postParamVal2: Option[String]): KoauthRequest = {
+    def appendIfExists(key: Option[String], value: Option[String], list: List[(String, String)]): List[(String, String)] = {
+      value match {
+        case None => list
+        case Some(someValue) =>
+          key match {
+            case None => list.::(("", someValue))
+            case Some(someKey) => list.::((someKey, someValue))
+        }
+      }
     }
 
     var list = List.empty[(String, String)]
-    list = appendIfExists(ConsumerKeyName, consumerKey, list)
-    list = appendIfExists(ConsumerSecretName, consumerSecret, list)
-    list = appendIfExists(TokenName, tokenName, list)
-    list = appendIfExists(TokenSecretName, tokenSecret, list)
-    list = appendIfExists(SignatureMethodName, signatureMethod, list)
-    list = appendIfExists(TimestampName, timestamp, list)
-    list = appendIfExists(NonceName, nonce, list)
-    list = appendIfExists(VersionName, version, list)
-    list = appendIfExists(CallbackName, callback, list)
-    list = appendIfExists(VerifierName, verifier, list)
+    list = appendIfExists(Some(ConsumerKeyName), consumerKey, list)
+    list = appendIfExists(Some(ConsumerSecretName), consumerSecret, list)
+    list = appendIfExists(Some(TokenName), tokenName, list)
+    list = appendIfExists(Some(TokenSecretName), tokenSecret, list)
+    list = appendIfExists(Some(SignatureMethodName), signatureMethod, list)
+    list = appendIfExists(Some(TimestampName), timestamp, list)
+    list = appendIfExists(Some(NonceName), nonce, list)
+    list = appendIfExists(Some(VersionName), version, list)
+    list = appendIfExists(Some(CallbackName), callback, list)
+    list = appendIfExists(Some(VerifierName), verifier, list)
 
     var bodyParamsList = List.empty[(String, String)]
     bodyParamsList = appendIfExists(postParamName1, postParamVal1, bodyParamsList)
     bodyParamsList = appendIfExists(postParamName2, postParamVal2, bodyParamsList)
 
-    KoauthRequest(method, url, List.empty, bodyParamsList, list)
+    KoauthRequest(method.getOrElse(""), url.getOrElse(""), List.empty, bodyParamsList, list)
   }
 
-  private def toGeneralForm(request: KoauthRequest): Option[(String, String, String, String, String, String,
-    String, String, String, String, String, String, String, String, String, String)] = {
-    Some(request.method,
-      request.urlWithoutParams,
-      request.oauthParamsMap(ConsumerKeyName),
-      request.oauthParamsMap(ConsumerSecretName),
-      request.oauthParamsMap(TokenName),
-      request.oauthParamsMap(TokenSecretName),
-      request.oauthParamsMap(SignatureMethodName),
-      request.oauthParamsMap(TimestampName),
-      request.oauthParamsMap(NonceName),
-      request.oauthParamsMap(VersionName),
-      request.oauthParamsMap(CallbackName),
-      request.oauthParamsMap(VerifierName),
-      request.bodyParams.applyOrElse(0, x => ("", ""))._1,
-      request.bodyParams.applyOrElse(0, x => ("", ""))._2,
-      request.bodyParams.applyOrElse(1, x => ("", ""))._1,
-      request.bodyParams.applyOrElse(1, x => ("", ""))._2
+  private def toGeneralForm(request: KoauthRequest): Option[(Option[String], Option[String], Option[String], Option[String], Option[String], Option[String],
+    Option[String], Option[String], Option[String], Option[String], Option[String], Option[String], Option[String], Option[String], Option[String], Option[String])] = {
+
+    def getOrElseBodyParams(i: Int) =
+      if (request.bodyParams.isDefinedAt(i)) (Some(request.bodyParams(i)._1), Some(request.bodyParams(i)._2))
+      else (None, None)
+
+    Some((Some(request.method),
+      Some(request.urlWithoutParams),
+      request.oauthParamsMap.get(ConsumerKeyName),
+      request.oauthParamsMap.get(ConsumerSecretName),
+      request.oauthParamsMap.get(TokenName),
+      request.oauthParamsMap.get(TokenSecretName),
+      request.oauthParamsMap.get(SignatureMethodName),
+      request.oauthParamsMap.get(TimestampName),
+      request.oauthParamsMap.get(NonceName),
+      request.oauthParamsMap.get(VersionName),
+      request.oauthParamsMap.get(CallbackName),
+      request.oauthParamsMap.get(VerifierName),
+      getOrElseBodyParams(0)._1,
+      getOrElseBodyParams(0)._2,
+      getOrElseBodyParams(1)._1,
+      getOrElseBodyParams(1)._2)
     )
   }
 }
